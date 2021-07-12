a34d2bbc2d641247ac02321cc78da72e299e2a22
kiiii8r <gkii.8r@gmail.com>
2021-07-12 14:30:12 +0900

    updat create view

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,10 +3,13 @@
 @section('content')
 <div class="card">
     <div class="card-header">新規メモ作成</div>
-    <form class="card-body" action="/store" method="POST">
+    <form class="card-body" action="{{ route('store') }}" method="POST">
+        @csrf
         <div class="mb-3">
             <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力"></textarea>
         </div>
+        <button type="submit" class="btn btn-primary">保存</button>
     </form>
 </div>
+
 @endsection
