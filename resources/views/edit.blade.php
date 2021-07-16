ac2c0f14519c78b59dea144e1bf9084092452489
kiiii8r <gkii.8r@gmail.com>
2021-07-16 14:26:24 +0900

    edit バリデーション

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -15,15 +15,18 @@
         @csrf
         <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
         <div class="mb-3">
-            <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
+            <textarea class="form-control" name="content" rows="3" maxlength="255" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
         </div>
+        @error('content')
+            <div class="alert alert-danger">メモ内容を入力してください</div>
+        @enderror
         @foreach($tags as $tag)
         <div class="form-check form-check-inline mb-3">
             <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}" {{ in_array($tag['id'], $include_tags) ? 'checked' : '' }}>
             <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
         </div>
         @endforeach
-        <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力"/>
+        <input type="text" class="form-control w-50 mb-3" name="new_tag" maxlength="18" placeholder="新しいタグを入力"/>
         <button type="submit" class="btn btn-primary">更新</button>
     </form>
 </div>
