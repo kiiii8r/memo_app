56c3de35ba7ff3dd9aab1830c5799de1f5ce3ef1
kiiii8r <gkii.8r@gmail.com>
2021-07-18 01:43:55 +0900

    edit 画面調整

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -11,15 +11,15 @@ crossorigin="anonymous"></script>
 @section('content')
 <div class="card">
 
-    <div class="card-header">メモ編集
-        <form class="card-body" id="delete-form" action="{{ route('destroy') }}" method="POST">
+    <div class="card-header d-flex justify-content-between">メモ編集
+        <form id="delete-form" action="{{ route('destroy') }}" method="POST">
             @csrf
             <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
-            <button type="submit" id="delete">削除</button>
+            <button class="delete" type="submit" id="delete"><i class="far fa-trash-alt"></i></button>
         </form>
     </div>
 
-    <form class="card-body" action="{{ route('update') }}" method="POST">
+    <form class="card-body my-card-body" action="{{ route('update') }}" method="POST">
         @csrf
         <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
         <div class="mb-3">
