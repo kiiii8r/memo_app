3e7f032959c1c8b12c72fb3cbff7c9972bdea0b2
kiiii8r <gkii.8r@gmail.com>
2021-07-22 07:37:07 +0900

    update create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,51 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card">
-    <div class="card-header">新規メモ作成</div>
-    <form class="card-body my-card-body" action="{{ route('store') }}" method="POST">
-        @csrf
-        <div class="mb-3">
-            <textarea class="form-control" name="content" rows="8" maxlength="255" placeholder="ここにメモを入力"></textarea>
+<div class="col-sm-12 col-md-4 p-0">
+   <div class="card">
+        <div class="card-header">メモ一覧
+            <a href="{{ route('index') }}" class="text-secondary"><i class="fas fa-plus"></i></a>
         </div>
-        @error('content')
-            <div class="alert alert-danger">メモ内容を入力してください</div>
-        @enderror
-        <div class="mb-3">
-            <input type="text" class="form-control" name="url" placeholder="YoutubeのURLを入力">
-        </div>
-        @error('url')
-        <div class="alert alert-danger">YoutubeのURLを入力してください</div>
-        @enderror
-        <div class="d-flex justify-content-start">
-            <div class="mb-3">
-                評価
-                <select name="score">
-                    @foreach(config('score') as $key => $score)
-                    <option value="{{ $key }}">{{ $score }}</option>
-                    @endforeach
-                </select>
+        <div class="card-body my-card-body">
+            <div class="mb-3 text-right border-bottom">
+                <a href="{{ route('search') }}" class="text-secondary">他ユーザーのメモ一覧へ</a>
             </div>
-            <div class="mb-3 ml-3">
-                ジャンル選択
-                <select name="genre">
-                    @foreach(config('genre') as $key => $genre)
-                    <option value="{{ $key }}">{{ $genre }}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-        <div class="scroll-overflow-create">
-            @foreach($tags as $tag)
-            <div class="form-check form-check-inline mb-3">
-                <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}">
-                <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
-            </div>
+            {{-- 各ユーザーのメモ一覧表示 --}}
+            @foreach($memos as $memo)
+                <a href="/edit/{{ $memo['id'] }}" class="card-text d-block ellipsis mb-2">{{ $memo['content'] }}</a>
             @endforeach
         </div>
-        <input type="text" class="form-control w-50 mb-3" name="new_tag" maxlength="18" placeholder="新しいタグを入力"/>
-        <button type="submit" class="btn btn-primary">保存</button>
-    </form>
+    </div>
+</div>
+
+<div class="col-sm-12 col-md-6 p-0">
+    <div class="card">
+        <div class="card-header">新規メモ作成</div>
+        <form class="card-body my-card-body" action="{{ route('store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <textarea class="form-control" name="content" rows="8" maxlength="255" placeholder="ここにメモを入力"></textarea>
+            </div>
+            @error('content')
+                <div class="alert alert-danger">メモ内容を入力してください</div>
+            @enderror
+            <div class="mb-3">
+                <input type="text" class="form-control" name="url" placeholder="YoutubeのURLを入力">
+            </div>
+            @error('url')
+            <div class="alert alert-danger">YoutubeのURLを入力してください</div>
+            @enderror
+            <div class="d-flex justify-content-start">
+                <div class="mb-3">
+                    評価
+                    <select name="score">
+                        @foreach(config('score') as $key => $score)
+                        <option value="{{ $key }}">{{ $score }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mb-3 ml-3">
+                    ジャンル選択
+                    <select name="genre">
+                        @foreach(config('genre') as $key => $genre)
+                        <option value="{{ $key }}">{{ $genre }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="scroll-overflow-create">
+                @foreach($tags as $tag)
+                <div class="form-check form-check-inline mb-3">
+                    <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}">
+                    <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
+                </div>
+                @endforeach
+            </div>
+            <input type="text" class="form-control w-50 mb-3" name="new_tag" maxlength="18" placeholder="新しいタグを入力"/>
+            <button type="submit" class="btn btn-primary">保存</button>
+        </form>
+    </div>
 </div>
 
 @endsection
