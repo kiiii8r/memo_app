6d12a684cc818902bafed64fadc148ede8dd4239
kiiii8r <gkii.8r@gmail.com>
2021-07-25 04:39:29 +0900

    add routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,6 @@ Route::post('/destroy', [HomeController::class, 'destroy'])->name('destroy');
 Route::post('/tag_destroy', [HomeController::class, 'tag_destroy'])->name('tag_destroy');
 Route::get('/search', [HomeController::class, 'search'])->name('search');
 Route::get('/read/{id}', [HomeController::class, 'read'])->name('read');
+Route::get('/mypage', [UserController::class, 'mypage'])->name('mypage');
+Route::get('/user/{id}', [UserController::class, 'user'])->name('user');
+Route::post('/user_update', [UserController::class, 'user_update'])->name('user_update');
