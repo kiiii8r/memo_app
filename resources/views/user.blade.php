fd3b861a9bdfe1ebc2e5b6881b88f4983b83785f
kiiii8r <gkii.8r@gmail.com>
2021-07-25 04:40:07 +0900

    add users

---
+++ resources/views/user.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('javascript')
+
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+crossorigin="anonymous"></script>
+<script src="/js/confirm.js"></script>
+
+@endsection
+
+@section('content')
+
+        <div class="card-header">ユーザーのプロファイル</div>
+        <div class="card-body my-card-body">
+        </div>
+
+@endsection
