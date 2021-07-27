ee76e88d24a9206391298f3a3bf3dc799f865edf
kiiii8r <gkii.8r@gmail.com>
2021-07-27 23:21:36 +0900

    adjustment edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -10,7 +10,8 @@ crossorigin="anonymous"></script>
 
 @section('content')
 
-<div class="card-header d-flex justify-content-between">メモ編集
+<div class="card-header d-flex justify-content-between"><span>メモ編集
+ <a href="/content/{{ $edit_memo[0]['id'] }}" class="text-secondary ml-1"><i class="fas fa-undo-alt"></i></a></span>
     <form id="delete-form" action="{{ route('destroy') }}" method="POST">
         @csrf
         <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
@@ -19,67 +20,71 @@ crossorigin="anonymous"></script>
 </div>
 
 <form class="card-body my-card-body" action="{{ route('update') }}" method="POST">
-    <div class="mb-2 d-flex justify-content-center">
-        <iframe width="65%" height="230" src="https://www.youtube.com/embed/{{$youtube}}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <div class="text-right ml-3">
-            <div class="mb-3">
-                <h5>評価</h5>
-                <select name="score">
-                    @foreach(config('score') as $key => $score)
-                        <option value="{{ $key }}"  {{ $edit_memo[0]['score'] === $key ? 'selected' : ''}}>{{ $score }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="mb-2">
-                <h5>ジャンル</h5>
-                <select name="genre">
-                    @foreach(config('genre') as $key => $genre)
-                        <option value="{{ $key }}"  {{ $edit_memo[0]['genre'] === $key ? 'selected' : '' }}>{{ $genre }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="mb-2">
-                <a href="{{ $edit_memo[0]['url'] }}" class="text-danger btn-lg"><i class="fab fa-youtube"></i></a>
-            </div>
-        </div>
-    </div>
     @csrf
     <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
-    <div class="mb-1">
-        <textarea class="form-control" name="content" rows="3" maxlength="500" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
+    <div class="mb-2">
+        <textarea class="form-control" name="content" rows="9" maxlength="500" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
     </div>
     @error('content')
         <div class="alert alert-danger">メモ内容を入力してください</div>
     @enderror
-    <div class="mb-1">
+    <div class="mb-2">
         <input type="text" class="form-control" name="url" placeholder="YoutubeのURLを入力" value="{{ $edit_memo[0]['url'] }}">
     </div>
     @error('url')
         <div class="alert alert-danger">YoutubeのURLを入力してください</div>
     @enderror
-    <div class="d-flex justify-content-start">
+    <div class="d-flex">
+        <div class="mb-2">
+        <div class="d-inline">評価：</div>
+        <select name="score">
+            @foreach(config('score') as $key => $score)
+                <option value="{{ $key }}"  {{ $edit_memo[0]['score'] === $key ? 'selected' : ''}}>{{ $score }}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="mb-2 ml-2">
+            <div class="d-inline">ジャンル：</div>
+        <select name="genre">
+            @foreach(config('genre') as $key => $genre)
+                <option value="{{ $key }}"  {{ $edit_memo[0]['genre'] === $key ? 'selected' : '' }}>{{ $genre }}</option>
+            @endforeach
+        </select>
+    </div>
 
     </div>
-    <div class="scroll-overflow-tag">
-        上位のタグ：
-        @foreach($all_tags as $tag)
-        <div class="form-check form-check-inline mb-2">
-            <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}" {{ in_array($tag['id'], $include_tags) ? 'checked' : '' }}>
-            <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
+    <div class="border-bottom d-flex">
+        @php($rank = 0)
+        <div class="w-50">人気のタグ</div>
+        <div class="scroll h-80px">
+            @foreach($all_tags as $tag)
+                <div class="form-check form-check-inline mb-2">
+                    @php($rank++)
+                    <label class="form-check-label btn btn-outline-success btn-sm" for="{{ $tag['id'] }}">
+                        @if(!(in_array($tag['id'], $include_tags)))
+                        <input class="form-check-input small" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}">
+                    @endif
+                        {{ $rank }}位：{{ $tag['name'] }}({{ $tag['count'] }})
+                    </label>
+                </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
-    <div class="scroll-overflow-tag">
-        マイタグ：
-        @foreach($tags as $tag)
-        <div class="form-check form-check-inline mb-2">
-            <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}" {{ in_array($tag['id'], $include_tags) ? 'checked' : '' }}>
-            <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
+    <div class="d-flex mt-1">
+        <div class="w-25">マイタグ</div>
+        <div class="scroll h-80px">
+            @foreach($tags as $tag)
+                <div class="form-check form-check-inline mb-2">
+                    <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}" {{ in_array($tag['id'], $my_tags) ? 'checked' : '' }}>
+                    <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
+                </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
     <input type="text" class="form-control w-50 mb-2" name="new_tag" maxlength="18" placeholder="新しいタグを入力"/>
-    <button type="submit" class="btn btn-primary">更新</button>
+    <div class="d-flex justify-content-end">
+        <button type="submit" class="btn btn-primary">更新</button>
+    </div>
 </form>
 
 @endsection
