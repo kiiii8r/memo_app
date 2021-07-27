e51388794f5a7b8dc3908be5c8916e04fdc8688e
kiiii8r <gkii.8r@gmail.com>
2021-07-28 00:28:23 +0900

    update read

--- resources/views/read.blade.php
+++ resources/views/read.blade.php
@@ -10,7 +10,7 @@ crossorigin="anonymous"></script>
 
 @section('content')
 
-<div class="card-header">{{ $other_memo[0]['user_name']}}さんのメモ内容</div>
+<div class="card-header"><a href="/user/{{ $other_memo[0]['user_id'] }}">{{ $other_memo[0]['user_name']}}</a>さんのメモ内容</div>
     <div class="card-body my-card-body">
     <div class="mb-2 d-flex justify-content-center">
         <iframe width="70%" height="250" src="https://www.youtube.com/embed/{{$youtube}}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -41,11 +41,11 @@ crossorigin="anonymous"></script>
         </div>
     </div>
 
-    <div class="m-3 scroll-overflow-read border-top border-bottom">
-        {{ $other_memo[0]['content'] }}
+    <div class="m-3 scroll h-220px border-top border-bottom">
+        {!!nl2br(e($other_memo[0]['content']))!!}
     </div>
 
-    <div class="scroll-overflow-tag">登録タグ:
+    <div class="scroll h-60px">登録タグ:
         @foreach($tags as $tag)
             @if(in_array($tag['id'], $include_tags))
                 <a class="ml-2" href="/search/?tag={{ $tag['id'] }}">
